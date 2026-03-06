bd0bf72ee6f2131ea6376287879ea39e949bbf41
samuelvalencia123123 <samuelvalencia7900@gmail.com>
2026-03-06 14:02:17 -0500

    Create codigo.py

---
+++ codigo.py
@@ -0,0 +1,12 @@
+import math
+
+radio = float(input("Escribe el radio del circulo:"))
+area1 = math.pi * radio**2
+print("El area es:",area1)
+
+base = float(input("Escribe la base del triangulo rectangulo:"))
+altura = float(input("Escribe la altura del triangulo resctangulo:"))
+area2 = (base * altura) / 2
+print("El area del triangulo rectangulo es:", area2)
+
+input("presiona enter para salir...")
